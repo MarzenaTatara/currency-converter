60c34938184d9ffb556d1999520202854cf823df
Marzena Tatara <marzenatatara77@gmail.com>
2023-05-16 15:16:51 +0200

    added switch in js

--- js/script.js
+++ js/script.js
@@ -2,25 +2,32 @@ let formElement = document.querySelector(".js-form");
 let amountElement = document.querySelector(".js-amount");
 let currencyElement = document.querySelector(".js-currency");
 let resultElement = document.querySelector(".js-result");
+let submitElement = document.querySelector(".js-submit");
+
+let rateEUR = 4.75;
+let rateUSD = 4.48;
+let rateEGP = 0.13;
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let amount = amountElement.value;
+    let amount = +amountElement.value;
     let currency = currencyElement.value;
-    let USD = 4.48;
-    let EUR = 4.75;
-    let EGP = 0.15;
+
     let result;
 
     switch (currency) {
-        case "USD": result = amount * USD
+        case "EUR":
+            result = amount * rateEUR;
             break;
-        case "EUR": result = amount * EUR
+
+        case "USD":
+            result = amount * rateUSD;
             break;
-        case "EGP": result = amount * EGP
+
+        case "EGP":
+            result = amount * rateEGP;
             break;
     }
-
-    resultElement.innerText = result.toFixed(2);
-});
+    resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
+})  
